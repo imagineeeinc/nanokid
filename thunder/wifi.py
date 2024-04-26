16f32b2146de11315f28108e3c46177b9c41acd6
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 <59028351+imagineeeinc@users.noreply.github.com>
2024-04-26 20:53:47 +0100

    added new wifi auto find and connect

--- thunder/wifi.py
+++ thunder/wifi.py
@@ -1,5 +1,5 @@
 import board, os
-import ssl, wifi, socketpool, adafruit_requests
+import ssl, wifi, socketpool, adafruit_requests, mdns
 
 class Wifi:
 	def __init__(self):
@@ -13,9 +13,30 @@ class Wifi:
 		self.password = None
 		self.requests = None
 
-	def connect(self, ssid=os.getenv("WIFI_SSID"), password=os.getenv("WIFI_PASSWORD")):
-		self.ssid = ssid
-		self.password = password
+	def connect(self, ssid=None, password=None):
+		if ssid != None and password != None:
+			self.ssid = ssid
+			self.password = password
+		else:
+			no = 0
+			try:
+				no = int(os.getenv("WIFIS"))
+			except(e):
+				pass
+			if no > 0:
+				scaned = self.scan_wifi()
+				for i in range(no):
+					if os.getenv("WIFI_SSID"+str(i)) in scaned:
+						self.ssid = os.getenv("WIFI_SSID"+str(i))
+						self.password = os.getenv("WIFI_PASSWORD"+str(i))
+						break
+				if self.ssid == None:
+					print("couldn't find a network to connect to")
+					return False
+			else:
+				self.ssid = os.getenv("WIFI_SSID")
+				self.password = os.getenv("WIFI_PASSWORD")
+
 		wifi.radio.start_station()
 		wifi.radio.connect(self.ssid, self.password)
 		if wifi.radio.connected:
@@ -25,6 +46,15 @@ class Wifi:
 		else:
 			self._connected = False
 			return False
+
+	def scan_wifi(self, sort_by=1, order=True):
+		access_points = []
+		# Scan for WiFi networks
+		for networks in (wifi.radio.start_scanning_networks()):
+			s = "."*(20-len(str(networks.ssid, "utf-8")))
+			access_points.append(networks.ssid)
+		wifi.radio.stop_scanning_networks()
+		return access_points
 
 	def create_pool(self):
 		self.pool = socketpool.SocketPool(wifi.radio)
@@ -41,3 +71,9 @@ class Wifi:
 			return self.requests.get(url)
 		except RuntimeError as er:
 			return er
+	
+	def mdns_setup(self, addr="nanokid", p=5000):
+		self.mdns_server = mdns.Server(self.wifi.radio)
+		self.mdns_server.hostname = addr
+		self.mdns_server.advertise_service(service_type="_http", protocol="_tcp", port=p)
+		self.mdns_addr=addr+".local:"+p
