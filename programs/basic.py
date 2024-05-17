72ef74a934ec2dec73d75d63e2137852a76d68e8
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 <59028351+imagineeeinc@users.noreply.github.com>
2024-05-17 17:05:13 +0100

    added new functionality to basic interpreter

--- programs/basic.py
+++ programs/basic.py
@@ -1,5 +1,5 @@
-# A custom basic interpreter
-import re, time, board, usb_cdc
+# A rudementry basic lang interpreter
+import time, board, usb_cdc, math, io
 import terminalio
 from adafruit_display_text import label
 from collections import OrderedDict
@@ -9,6 +9,7 @@ from thunder.display import Display
 from thunder.mount_sd import init_sdcard
 import thunder.input as controls
 import thunder.utils as u
+import thunder.sys as sys
 
 init_sdcard()
 
@@ -26,6 +27,22 @@ var = {}
 
 s = usb_cdc.console
 
+def read_serial(serial):
+  available = serial.in_waiting
+  text = ""
+  while available:
+    raw = serial.read(available)
+    text = raw.decode("utf-8")
+    available = serial.in_waiting
+  return text
+
+def sort(code):
+  myKeys = list(code.keys())
+  myKeys.sort()
+  res = OrderedDict()
+  for i in myKeys:
+    res[i] = code[i]
+  return res
 def checkDigit(str):
   res=False
   for i in str:
@@ -37,6 +54,7 @@ def checkDigit(str):
       res=False
   return res
 def parse(tree):
+  global code
   n = 0
   val = 0
   while n <= len(tree)-1:
@@ -56,7 +74,7 @@ def parse(tree):
         string = var[tree[0]]
       else:
         string = " ".join(tree)
-      string = u.divide_chunks(string, 40)
+      string = u.divide_chunks(str(string), 40)
       for i in string:
         term.text += i+"\n"
     if i == "scale":
@@ -75,20 +93,20 @@ def parse(tree):
     if i == "run":
       j = 0
       l = list(code.keys())
-      while j <= (len(code)-1):
+      while j < len(code):
         tree = code[l[j]].split(" ")
         res = parse(tree)
         if res != 0:
           j = l.index(res)+1
         else:
           j+=1
-        k = s.read(2)
-        if k == b'cc':
+        k = read_serial(s)
+        if k == "C":
           break
       break
     if i == "goto":
       if checkDigit(tree[n+1]):
-        nn = tree[n+1]
+        nn = int(tree[n+1])
         tree = code[nn].split(" ")
         parse(tree)
         val = nn
@@ -97,21 +115,56 @@ def parse(tree):
       if checkDigit(tree[n+1]):
         time.sleep(float(tree[n+1]))
       break
-    if i == "list":
+    if i == "ls" or i == "list":
       j = 0
       l = list(code.keys())
       while j <= (len(code)-1):
-        print(l[j]+" "+code[l[j]])
+        print(str(l[j])+" "+code[l[j]])
         j+=1
       break
+    if i == "ld" or i == "load":
+      content = io.open(tree[1], mode='r').read()
+      prog_tree = content.split("\n")
+      for pos in range(0, len(prog_tree)):
+        code[pos+1] = prog_tree[pos]
+        code = sort(code)
+      break
+    if i == "shift":
+      if checkDigit(tree[n+1]):
+        # take all the lines strored in code and shift the kets by tree[n+1]
+        l = list(code.keys())
+        temp = OrderedDict()
+        for pos in range(0, len(l)):
+          temp[l[pos]+int(tree[n+1])] = code[l[pos]]
+        code = sort(temp)
+        break
+    if i == "exit":
+      val = -1
+      break
+    if i == "mv":
+      if int(tree[1]) in code:
+        code[int(tree[2])] = code[int(tree[1])]
+        code.pop(int(tree[1]))
+        code = sort(code)
+        break
+    if i == "del":
+      if int(tree[1]) in code:
+        code.pop(int(tree[1]))
+        break
     if checkDigit(i):
       del tree[0]
-      code[i] = " ".join(tree)
+      code[int(i)] = " ".join(tree)
+      code = sort(code)
       break
-    elif i[0] == "$" and tree[1] == "=":
-      del tree[0]
-      del tree[0]
-      var[i] = " ".join(tree)
+    elif i[0] == "$":
+      if tree[1] == "=":
+        del tree[0]
+        del tree[0]
+        var[i] = " ".join(tree)
+      elif tree[1] == "mat":
+        del tree[0]
+        del tree[0]
+        var[i] = eval(" ".join(tree))
     n+=1
   return val
 def main():
@@ -119,6 +172,8 @@ def main():
   while True:
     inp = input(">>> ")
     tree = inp.split(" ")
-    parse(tree)
-
+    status = parse(tree)
+    if status == -1:
+      break
 main()
+sys.exit_to_shell()
